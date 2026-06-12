5fb080b57f77b1f08cb3421eea19c53ea5c4df6c
Saran M K <mksaran0@gmail.com>
2026-06-13 02:32:31 +0530

    flutter project is created

---
+++ offlinenotesapp/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(OfflineNotesApp());
+}
+
+class OfflineNotesApp extends StatelessWidget {
+  const OfflineNotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
